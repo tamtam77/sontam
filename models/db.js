9a6ad4fe4b3ea0baad4100dd6bd95040e2293a1f
Ngoc Tam <tamsn99@fpt.edu.vn>
2021-07-12 09:29:45 +0700

    first commit

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-const url = "mongodb+srv://phandat:dat112233@cluster0.tbaux.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const url = "mongodb+srv://ngoctam:tam123123@cluster0.8b0yh.mongodb.net/myFirstDatabase?retryWrites=true&w=majoritymongodb+srv://ngoctam:tam123123@cluster0.8b0yh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 mongoose.connect(url,{useNewUrlParser:true},(err) => {
     if(!err){ console.log("MongoDB Connection Succeeded");}
     else{
